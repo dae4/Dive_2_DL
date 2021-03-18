feede79939ca29cb0c3ccdc9845a2949535fedc5
dae4 <3210m0123@gmail.com>
2021-03-18 20:53:44 +0900

    proto

---
+++ Chapter9/gpu_concise.py
@@ -0,0 +1,6 @@
+import tensorflow as tf
+from func import *
+gru_later = 
+model = Rnnmodel(gru_later,len(Vocab))
+
+train_rnn(model,train_iter,vocab,num_hiddens,lr,num_epochs,strategy,get_params, use_random_iter=True)
