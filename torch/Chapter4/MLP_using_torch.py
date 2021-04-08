ba93010935305499ffba2bde332c20932e571718
dae4 <3210m0123@gmail.com>
2021-04-08 16:13:09 +0900

    using torch

---
+++ torch/Chapter4/MLP_using_torch.py
@@ -0,0 +1,23 @@
+#%%
+import torch
+from func import *
+from torch import nn
+
+net = nn.Sequential(nn.Flatten(),
+                    nn.Linear(784, 256),
+                    nn.ReLU(),
+                    nn.Linear(256, 10))
+
+def init_weights(m):
+    if type(m) == nn.Linear:
+        nn.init.normal_(m.weight, std=0.01)
+
+net.apply(init_weights)
+
+batch_size, lr, num_epochs = 256, 0.1, 10
+loss = nn.CrossEntropyLoss()
+trainer = torch.optim.SGD(net.parameters(), lr=lr)
+
+train_iter, test_iter = load_data_fashion_mnist(batch_size)
+train(net, train_iter, test_iter, loss, num_epochs, trainer)
+# %%
