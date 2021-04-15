4a0f4810f24dbf444e465f88ca639a15e3f45dbd
dae4 <3210m0123@gmail.com>
2021-04-15 18:54:34 +0900

    convolution

---
+++ torch/Chapter6/conv.py
@@ -0,0 +1,60 @@
+#%%
+import torch 
+from torch import nn
+
+def corr2d(X, K):  #@save
+    """Compute 2D cross-correlation."""
+    h, w = K.shape
+    Y = torch.zeros((X.shape[0] - h + 1, X.shape[1] - w + 1))
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            Y[i, j] = torch.sum((X[i: i + h, j: j + w] * K))
+    return Y
+
+X = torch.tensor([[0.0, 1.0, 2.0], [3.0, 4.0, 5.0], [6.0, 7.0, 8.0]])
+K = torch.tensor([[0.0, 1.0], [2.0, 3.0]])
+corr2d(X, K)
+# %%
+class Conv2D(nn.Module):
+    def __init__(self, kernel_size):
+        super().__init__()
+        self.weight = nn.Parameter(torch.rand(kernel_size))
+        self.bias = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x):
+        return corr2d(x, self.weight) + self.bias
+#%%
+X = torch.ones((6, 8))
+X[:, 2:6] = 0
+X
+# %%
+K = torch.tensor([[1.0, -1.0]])
+# %%
+Y = corr2d(X, K)
+Y
+# %%
+corr2d(torch.transpose(X,0,1), K)
+# %%
+# Construct a two-dimensional convolutional layer with 1 output channel and a
+# kernel of shape (1, 2). For the sake of simplicity, we ignore the bias here
+conv2d = nn.Conv2d(1,1, kernel_size=(1, 2), bias=False)
+
+# The two-dimensional convolutional layer uses four-dimensional input and
+# output in the format of (example channel, height, width), where the batch
+# size (number of examples in the batch) and the number of channels are both 1
+X = X.reshape((1, 1, 6, 8))
+Y = Y.reshape((1, 1, 6, 7))
+lr = 3e-2  # Learning rate
+
+for i in range(10):
+    Y_hat = conv2d(X)
+    l = (Y_hat - Y) ** 2
+    conv2d.zero_grad()
+    l.sum().backward()
+    # Update the kernel
+    conv2d.weight.data[:] -= lr * conv2d.weight.grad
+    if (i + 1) % 2 == 0:
+        print(f'batch {i + 1}, loss {l.sum():.3f}')
+# %%
+torch.reshape(conv2d.weight.data, (1, 2))
+# %%
