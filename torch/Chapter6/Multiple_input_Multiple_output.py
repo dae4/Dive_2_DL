9be78caf1d8fe3417da0eff0f6c29b5623fd7ded
dae4 <3210m0123@gmail.com>
2021-04-16 21:16:08 +0900

    MI MO

---
+++ torch/Chapter6/Multiple_input_Multiple_output.py
@@ -0,0 +1,37 @@
+#%%
+import torch
+from func import *
+from torch import nn
+def corr2d_multi_in(X, K):
+    return sum(corr2d(x, k) for x, k in zip(X, K))
+
+X = torch.tensor([[[0.0, 1.0, 2.0], [3.0, 4.0, 5.0], [6.0, 7.0, 8.0]],
+               [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]])
+K = torch.tensor([[[0.0, 1.0], [2.0, 3.0]], [[1.0, 2.0], [3.0, 4.0]]])
+
+corr2d_multi_in(X, K)
+#%%
+
+def corr2d_multi_in_out(X, K):
+    return torch.stack([corr2d_multi_in(X, k) for k in K], 0)
+
+K = torch.stack((K, K + 1, K + 2), 0)
+K.shape
+
+corr2d_multi_in_out(X, K)
+#%%
+def corr2d_multi_in_out_1x1(X, K):
+    c_i, h, w = X.shape
+    c_o = K.shape[0]
+    X = torch.reshape(X, (c_i, h * w))
+    K = torch.reshape(K, (c_o, c_i))
+    Y = torch.matmul(K, X)
+    return torch.reshape(Y, (c_o, h, w))
+
+X = torch.normal(0, 1, (3, 3, 3))
+K = torch.normal(0, 1, (2, 3, 1, 1))
+
+Y1 = corr2d_multi_in_out_1x1(X, K)
+Y2 = corr2d_multi_in_out(X, K)
+assert float(torch.sum(torch.abs(Y1 - Y2))) < 1e-6
+# %%
