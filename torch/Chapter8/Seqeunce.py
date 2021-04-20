eed2611a104ec64d35bb527aa5f82c59287ef53a
dae4 <3210m0123@gmail.com>
2021-04-20 18:26:44 +0900

    prediction

--- torch/Chapter8/Seqeunce.py
+++ torch/Chapter8/Seqeunce.py
@@ -49,3 +49,36 @@ def train(net, train_iter, loss, epochs, lr):
 net = get_net()
 train(net, train_iter, loss, 5, 0.01)
 # %%
+onestep_preds = net(features)
+plot([time, time[tau:]], [torch.tensor(x), torch.tensor(onestep_preds)], 'time',
+         'x', legend=['data', '1-step preds'], xlim=[1, 1000], figsize=(6, 3))
+
+multistep_preds = torch.zeros(T)
+multistep_preds[: n_train + tau] = x[: n_train + tau]
+for i in range(n_train + tau, T):
+    multistep_preds[i] = net(
+        torch.reshape(multistep_preds[i - tau: i], (1, -1)))
+#%%
+plot([time, time[tau:], time[n_train + tau:]],
+         [torch.tensor(x), torch.tensor(onestep_preds),
+          torch.tensor(multistep_preds[n_train + tau:])], 'time',
+         'x', legend=['data', '1-step preds', 'multistep preds'],
+         xlim=[1, 1000], figsize=(6, 3))
+# %%
+max_steps = 64
+features = torch.zeros((T - tau - max_steps + 1, tau + max_steps))
+# Column `i` (`i` < `tau`) are observations from `x` for time steps from
+# `i + 1` to `i + T - tau - max_steps + 1`
+for i in range(tau):
+    features[:, i] = x[i: i + T - tau - max_steps + 1]
+
+# Column `i` (`i` >= `tau`) are the (`i - tau + 1`)-step-ahead predictions for
+# time steps from `i + 1` to `i + T - tau - max_steps + 1`
+for i in range(tau, tau + max_steps):
+    features[:, i] = torch.reshape(net(features[:, i - tau: i]), (1,-1))
+#%%
+steps = (1, 4, 16, 64)
+plot([time[tau + i - 1: T - max_steps + i] for i in steps],
+         [torch.tensor(features[:, tau + i - 1]) for i in steps], 'time', 'x',
+         legend=[f'{i}-step preds' for i in steps], xlim=[5, 1000],
+         figsize=(6, 3))
