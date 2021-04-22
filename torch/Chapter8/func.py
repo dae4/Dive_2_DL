02be07377d097ccdc7f1e9b1f2a1fef5cbd1f25c
dae4 <3210m0123@gmail.com>
2021-04-22 20:05:24 +0900

    rnn

--- torch/Chapter8/func.py
+++ torch/Chapter8/func.py
@@ -8,6 +8,10 @@ import requests
 import collections
 import re
 import random
+import numpy as np
+import torch.nn as nn
+import time
+import math
 
 def use_svg_display():
     """Use the svg format to display a plot in Jupyter."""
@@ -228,7 +232,7 @@ class SeqDataLoader:  #@save
     """An iterator to load sequence data."""
     def __init__(self, batch_size, num_steps, use_random_iter, max_tokens):
         if use_random_iter:
-        self.corpus, self.vocab = load_corpus_time_machine(max_tokens)
+            self.corpus, self.vocab = load_corpus_time_machine(max_tokens)
             self.data_iter_fn = seq_data_iter_random
         else:
             self.data_iter_fn = seq_data_iter_sequential
@@ -242,3 +246,185 @@ def load_data_time_machine(batch_size, num_steps,  use_random_iter=False, max_to
     """Return the iterator and the vocabulary of the time machine dataset."""
     data_iter = SeqDataLoader( batch_size, num_steps, use_random_iter, max_tokens)
     return data_iter, data_iter.vocab
+
+def try_gpu(i=0): 
+    """Return gpu(i) if exists, otherwise return cpu()."""
+    if torch.cuda.device_count() >= i + 1:
+        return torch.device(f'cuda:{i}')
+    return torch.device('cpu')
+
+def predict(prefix, num_preds, net, vocab, device):  
+    state = net.begin_state(batch_size=1, device=device)
+    outputs = [vocab[prefix[0]]]
+    get_input = lambda: torch.reshape(torch.tensor(
+        [outputs[-1]], device=device), (1, 1))
+    for y in prefix[1:]:  # Warm-up period
+        _, state = net(get_input(), state)
+        outputs.append(vocab[y])
+    for _ in range(num_preds):  # Predict `num_preds` steps
+        y, state = net(get_input(), state)
+        outputs.append(int(y.argmax(dim=1).reshape(1)))
+    return ''.join([vocab.idx_to_token[i] for i in outputs])
+
+class Accumulator:  #@save
+    """For accumulating sums over `n` variables."""
+    def __init__(self, n):
+        self.data = [0.0] * n
+
+    def add(self, *args):
+        self.data = [a + float(b) for a, b in zip(self.data, args)]
+
+    def reset(self):
+        self.data = [0.0] * len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
+    """Set the axes for matplotlib."""
+    axes.set_xlabel(xlabel)
+    axes.set_ylabel(ylabel)
+    axes.set_xscale(xscale)
+    axes.set_yscale(yscale)
+    axes.set_xlim(xlim)
+    axes.set_ylim(ylim)
+    if legend:
+        axes.legend(legend)
+    axes.grid()
+
+
+class Animator:  #@save
+    """For plotting data in animation."""
+    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None,
+                 ylim=None, xscale='linear', yscale='linear',
+                 fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1,
+                 figsize=(3.5, 2.5)):
+        # Incrementally plot multiple lines
+        if legend is None:
+            legend = []
+        use_svg_display()
+        self.fig, self.axes = plt.subplots(nrows, ncols, figsize=figsize)
+        if nrows * ncols == 1:
+            self.axes = [self.axes, ]
+        # Use a lambda function to capture arguments
+        self.config_axes = lambda: set_axes(
+            self.axes[0], xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+        self.X, self.Y, self.fmts = None, None, fmts
+
+    def add(self, x, y):
+        # Add multiple data points into the figure
+        if not hasattr(y, "__len__"):
+            y = [y]
+        n = len(y)
+        if not hasattr(x, "__len__"):
+            x = [x] * n
+        if not self.X:
+            self.X = [[] for _ in range(n)]
+        if not self.Y:
+            self.Y = [[] for _ in range(n)]
+        for i, (a, b) in enumerate(zip(x, y)):
+            if a is not None and b is not None:
+                self.X[i].append(a)
+                self.Y[i].append(b)
+        self.axes[0].cla()
+        for x, y, fmt in zip(self.X, self.Y, self.fmts):
+            self.axes[0].plot(x, y, fmt)
+        self.config_axes()
+        display.display(self.fig)
+        display.clear_output(wait=True)
+
+class Timer:
+    """Record multiple running times."""
+    def __init__(self):
+        self.times = []
+        self.start()
+
+    def start(self):
+        """Start the timer."""
+        self.tik = time.time()
+
+    def stop(self):
+        """Stop the timer and record the time in a list."""
+        self.times.append(time.time() - self.tik)
+        return self.times[-1]
+
+    def avg(self):
+        """Return the average time."""
+        return sum(self.times) / len(self.times)
+
+    def sum(self):
+        """Return the sum of time."""
+        return sum(self.times)
+
+    def cumsum(self):
+        """Return the accumulated time."""
+        return np.array(self.times).cumsum().tolist()
+
+def grad_clipping(net, theta):
+    """Clip the gradient."""
+    if isinstance(net, nn.Module):
+        params = [p for p in net.parameters() if p.requires_grad]
+    else:
+        params = net.params
+    norm = torch.sqrt(sum(torch.sum((p.grad ** 2)) for p in params))
+    if norm > theta:
+        for param in params:
+            param.grad[:] *= theta / norm
+
+def train_epoch(net, train_iter, loss, updater, device, use_random_iter):
+    """Train a net within one epoch (defined in Chapter 8)."""
+    state, timer = None, Timer()
+    metric = Accumulator(2)  # Sum of training loss, no. of tokens
+    for X, Y in train_iter:
+        if state is None or use_random_iter:
+            # Initialize `state` when either it is the first iteration or
+            # using random sampling
+            state = net.begin_state(batch_size=X.shape[0], device=device)
+        else:
+            if isinstance(net, nn.Module) and not isinstance(state, tuple):
+                # `state` is a tensor for `nn.GRU`
+                state.detach_()
+            else:
+                # `state` is a tuple of tensors for `nn.LSTM` and
+                # for our custom scratch implementation 
+                for s in state:
+                    s.detach_()
+        y = Y.T.reshape(-1)
+        X, y = X.to(device), y.to(device)
+        y_hat, state = net(X, state)
+        l = loss(y_hat, y.long()).mean()
+        if isinstance(updater, torch.optim.Optimizer):
+            updater.zero_grad()
+            l.backward()
+            grad_clipping(net, 1)
+            updater.step()
+        else:
+            l.backward()
+            grad_clipping(net, 1)
+            # Since the `mean` function has been invoked
+            updater(batch_size=1)
+        metric.add(l * len(y), len(y))
+    return math.exp(metric[0] / metric[1]), metric[1] / timer.stop()
+
+def train(net, train_iter, vocab, lr, num_epochs, device,
+              use_random_iter=False):
+    """Train a model (defined in Chapter 8)."""
+    loss = nn.CrossEntropyLoss()
+    animator = Animator(xlabel='epoch', ylabel='perplexity',
+                            legend=['train'], xlim=[10, num_epochs])
+    # Initialize
+    if isinstance(net, nn.Module):
+        updater = torch.optim.SGD(net.parameters(), lr)
+    else:
+        updater = lambda batch_size: torch.sgd(net.params, lr, batch_size)
+    predict = lambda prefix: predict(prefix, 50, net, vocab, device)
+    # Train and predict
+    for epoch in range(num_epochs):
+        ppl, speed = train_epoch(
+            net, train_iter, loss, updater, device, use_random_iter)
+        if (epoch + 1) % 10 == 0:
+            print(predict('time traveller'))
+            animator.add(epoch + 1, [ppl])
+    print(f'perplexity {ppl:.1f}, {speed:.1f} tokens/sec on {str(device)}')
+    print(predict('time traveller'))
+    print(predict('traveller'))
--- torch/Chapter8/rnn_scratch.py
+++ torch/Chapter8/rnn_scratch.py
@@ -1,7 +1,161 @@
+#%%
 import torch
 from torch import nn
 from torch.nn import functional as F
 import math
+from func import *
 
 batch_size, num_steps = 32, 35
 train_iter, vocab = load_data_time_machine(batch_size, num_steps)
+
+F.one_hot(torch.tensor([0, 2]), len(vocab))
+
+X = torch.reshape(torch.arange(10), (2, 5))
+F.one_hot(X.T, 28).shape
+#%%
+def get_params(vocab_size, num_hiddens, device):
+    num_inputs = num_outputs = vocab_size
+
+    def normal(shape):
+        return torch.randn(size=shape, device=device) * 0.01
+
+    # Hidden layer parameters
+    W_xh = normal((num_inputs, num_hiddens))
+    W_hh = normal((num_hiddens, num_hiddens))
+    b_h = torch.zeros(num_hiddens, device=device)
+    # Output layer parameters
+    W_hq = normal((num_hiddens, num_outputs))
+    b_q = torch.zeros(num_outputs, device=device)
+    # Attach gradients
+    params = [W_xh, W_hh, b_h, W_hq, b_q]
+    for param in params:
+        param.requires_grad_(True)
+    return params
+
+def init_rnn_state(batch_size, num_hiddens, device):
+    return (torch.zeros((batch_size, num_hiddens), device=device),)
+
+def rnn(inputs, state, params):
+    # Here `inputs` shape: (`num_steps`, `batch_size`, `vocab_size`)
+    W_xh, W_hh, b_h, W_hq, b_q = params
+    H, = state
+    outputs = []
+    # Shape of `X`: (`batch_size`, `vocab_size`)
+    for X in inputs:
+        H = torch.tanh(torch.mm(X, W_xh) + torch.mm(H, W_hh) + b_h)
+        Y = torch.mm(H, W_hq) + b_q
+        outputs.append(Y)
+    return torch.cat(outputs, dim=0), (H,)
+
+class RNNModelScratch: 
+    """A RNN Model implemented from scratch."""
+    def __init__(self, vocab_size, num_hiddens, device,
+                 get_params, init_state, forward_fn):
+        self.vocab_size, self.num_hiddens = vocab_size, num_hiddens
+        self.params = get_params(vocab_size, num_hiddens, device)
+        self.init_state, self.forward_fn = init_state, forward_fn
+
+    def __call__(self, X, state):
+        X = F.one_hot(X.T, self.vocab_size).type(torch.float32)
+        return self.forward_fn(X, state, self.params)
+
+    def begin_state(self, batch_size, device):
+        return self.init_state(batch_size, self.num_hiddens, device)
+
+num_hiddens = 512
+net = RNNModelScratch(len(vocab), num_hiddens, try_gpu(), get_params,
+                      init_rnn_state, rnn)
+state = net.begin_state(X.shape[0], try_gpu())
+Y, new_state = net(X.to(try_gpu()), state)
+Y.shape, len(new_state), new_state[0].shape
+#%%
+def predict(prefix, num_preds, net, vocab, device):  
+    state = net.begin_state(batch_size=1, device=device)
+    outputs = [vocab[prefix[0]]]
+    get_input = lambda: torch.reshape(torch.tensor(
+        [outputs[-1]], device=device), (1, 1))
+    for y in prefix[1:]:  # Warm-up period
+        _, state = net(get_input(), state)
+        outputs.append(vocab[y])
+    for _ in range(num_preds):  # Predict `num_preds` steps
+        y, state = net(get_input(), state)
+        outputs.append(int(y.argmax(dim=1).reshape(1)))
+    return ''.join([vocab.idx_to_token[i] for i in outputs])
+
+predict('time traveller ', 10, net, vocab)
+
+def grad_clipping(net, theta):
+    """Clip the gradient."""
+    if isinstance(net, nn.Module):
+        params = [p for p in net.parameters() if p.requires_grad]
+    else:
+        params = net.params
+    norm = torch.sqrt(sum(torch.sum((p.grad ** 2)) for p in params))
+    if norm > theta:
+        for param in params:
+            param.grad[:] *= theta / norm
+
+def train_epoch(net, train_iter, loss, updater, device, use_random_iter):
+    """Train a net within one epoch (defined in Chapter 8)."""
+    state, timer = None, Timer()
+    metric = Accumulator(2)  # Sum of training loss, no. of tokens
+    for X, Y in train_iter:
+        if state is None or use_random_iter:
+            # Initialize `state` when either it is the first iteration or
+            # using random sampling
+            state = net.begin_state(batch_size=X.shape[0], device=device)
+        else:
+            if isinstance(net, nn.Module) and not isinstance(state, tuple):
+                # `state` is a tensor for `nn.GRU`
+                state.detach_()
+            else:
+                # `state` is a tuple of tensors for `nn.LSTM` and
+                # for our custom scratch implementation 
+                for s in state:
+                    s.detach_()
+        y = Y.T.reshape(-1)
+        X, y = X.to(device), y.to(device)
+        y_hat, state = net(X, state)
+        l = loss(y_hat, y.long()).mean()
+        if isinstance(updater, torch.optim.Optimizer):
+            updater.zero_grad()
+            l.backward()
+            grad_clipping(net, 1)
+            updater.step()
+        else:
+            l.backward()
+            grad_clipping(net, 1)
+            # Since the `mean` function has been invoked
+            updater(batch_size=1)
+        metric.add(l * len(y), len(y))
+    return math.exp(metric[0] / metric[1]), metric[1] / timer.stop()
+
+def train(net, train_iter, vocab, lr, num_epochs, device,
+              use_random_iter=False):
+    """Train a model (defined in Chapter 8)."""
+    loss = nn.CrossEntropyLoss()
+    animator = Animator(xlabel='epoch', ylabel='perplexity',
+                            legend=['train'], xlim=[10, num_epochs])
+    # Initialize
+    if isinstance(net, nn.Module):
+        updater = torch.optim.SGD(net.parameters(), lr)
+    else:
+        updater = lambda batch_size: torch.sgd(net.params, lr, batch_size)
+    predict = lambda prefix: predict(prefix, 50, net, vocab, device)
+    # Train and predict
+    for epoch in range(num_epochs):
+        ppl, speed = train_epoch(
+            net, train_iter, loss, updater, device, use_random_iter)
+        if (epoch + 1) % 10 == 0:
+            print(predict('time traveller'))
+            animator.add(epoch + 1, [ppl])
+    print(f'perplexity {ppl:.1f}, {speed:.1f} tokens/sec on {str(device)}')
+    print(predict('time traveller'))
+    print(predict('traveller'))
+
+
+num_epochs, lr = 500, 1
+train(net, train_iter, vocab, lr, num_epochs, try_gpu())
+#%%
+net = RNNModelScratch(len(vocab), num_hiddens, try_gpu(), get_params, init_rnn_state, rnn)
+train(net, train_iter, vocab, lr, num_epochs, try_gpu(), use_random_iter=True)
