bdcabcfdf3e4ed4365e55d1cf1280f33fc386ded
dae4 <3210m0123@gmail.com>
2021-04-30 23:48:41 +0900

    revise

--- torch/Chapter10/Nadaraya-Watson_Regression.py
+++ torch/Chapter10/Nadaraya-Watson_Regression.py
@@ -1,2 +1,82 @@
+#%%
 import torch
 from torch import nn
+
+n_train = 50  # No. of training examples
+x_train, _ = torch.sort(torch.rand(n_train) * 5)   # Training inputs
+# %%
+
+def f(x):
+    return 2 * torch.sin(x) + x**0.8
+
+y_train = f(x_train) + torch.normal(0.0, 0.5, (n_train,))  # Training outputs
+x_test = torch.arange(0, 5, 0.1)  # Testing examples
+y_truth = f(x_test)  # Ground-truth outputs for the testing examples
+n_test = len(x_test)  # No. of testing examples
+n_test
+# %%
+import matplotlib.pyplot as plt
+from IPython import display
+
+def use_svg_display():
+    """Use the svg format to display a plot in Jupyter."""
+    display.set_matplotlib_formats('svg')
+
+def set_figsize(figsize=(3.5, 2.5)):
+    """Set the figure size for matplotlib."""
+    use_svg_display()
+    plt.rcParams['figure.figsize'] = figsize
+
+
+# Defined in file: ./chapter_preliminaries/calculus.md
+def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
+    """Set the axes for matplotlib."""
+    axes.set_xlabel(xlabel)
+    axes.set_ylabel(ylabel)
+    axes.set_xscale(xscale)
+    axes.set_yscale(yscale)
+    axes.set_xlim(xlim)
+    axes.set_ylim(ylim)
+    if legend:
+        axes.legend(legend)
+    axes.grid()
+
+def plot(X, Y=None, xlabel=None, ylabel=None, legend=None, xlim=None,
+         ylim=None, xscale='linear', yscale='linear',
+         fmts=('-', 'm--', 'g-.', 'r:'), figsize=(3.5, 2.5), axes=None):
+    """Plot data points."""
+    if legend is None:
+        legend = []
+
+    set_figsize(figsize)
+    axes = axes if axes else plt.gca()
+
+    # Return True if `X` (tensor or list) has 1 axis
+    def has_one_axis(X):
+        return (hasattr(X, "ndim") and X.ndim == 1 or
+                isinstance(X, list) and not hasattr(X[0], "__len__"))
+
+    if has_one_axis(X):
+        X = [X]
+    if Y is None:
+        X, Y = [[]] * len(X), X
+    elif has_one_axis(Y):
+        Y = [Y]
+    if len(X) != len(Y):
+        X = X * len(Y)
+    axes.cla()
+    for x, y, fmt in zip(X, Y, fmts):
+        if len(x):
+            axes.plot(x, y, fmt)
+        else:
+            axes.plot(y, fmt)
+    set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend)
+
+def plot_kernel_reg(y_hat):
+    plot(x_test, [y_truth, y_hat], 'x', 'y', legend=['Truth', 'Pred'],
+             xlim=[0, 5], ylim=[-1, 5])
+    plt.plot(x_train, y_train, 'o', alpha=0.5)
+# %%
+y_hat = torch.repeat_interleave(y_train.mean(), n_test)
+plot_kernel_reg(y_hat)
+# %%
