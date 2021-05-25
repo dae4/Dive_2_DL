1e863b7c4a348a54b441d2525060e23ab92fe70a
dae4 <3210m0123@gmail.com>
2021-05-25 20:42:08 +0900

    Anchor_Boxes.py

--- torch/Chapter13/func.py
+++ torch/Chapter13/func.py
@@ -373,3 +373,12 @@ def box_center_to_corner(boxes):
     y2 = cy + 0.5 * h
     boxes = torch.stack((x1, y1, x2, y2), axis=-1)
     return boxes
+
+def bbox_to_rect(bbox, color):
+    """Convert bounding box to matplotlib format."""
+    # Convert the bounding box (upper-left x, upper-left y, lower-right x,
+    # lower-right y) format to the matplotlib format: ((upper-left x,
+    # upper-left y), width, height)
+    return plt.Rectangle(
+        xy=(bbox[0], bbox[1]), width=bbox[2]-bbox[0], height=bbox[3]-bbox[1],
+        fill=False, edgecolor=color, linewidth=2)
