c409751c93f59345f7999ab7549a120cee217c9a
dae4 <3210m0123@gmail.com>
2021-05-28 23:44:29 +0900

    ssd.py

---
+++ torch/Chapter13/ssd.py
@@ -0,0 +1,13 @@
+import torch
+import torchvision
+from torch import nn
+from torch.nn import functional as F
+from func import *
+
+def cls_predictor(num_inputs, num_anchors, num_classes):
+    return nn.Conv2d(num_inputs, num_anchors * (num_classes + 1),
+                     kernel_size=3, padding=1)
+
+def bbox_predictor(num_inputs, num_anchors):
+    return nn.Conv2d(num_inputs, num_anchors * 4, kernel_size=3, padding=1)
+
