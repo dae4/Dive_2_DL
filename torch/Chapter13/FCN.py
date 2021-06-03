7df103a120b72a51dc6543ae2cce13b368d35407
dae4 <3210m0123@gmail.com>
2021-06-03 22:18:06 +0900

    FCN.py

--- torch/Chapter13/FCN.py
+++ torch/Chapter13/FCN.py
@@ -1,3 +1,10 @@
-import torch 
-import torch.nn
+import torch
+import torchvision
+from torch import nn
+from torch.nn import functional as F
+
 from func import *
+
+
+pretrained_net = torchvision.models.resnet18(pretrained=True)
+pretrained_net.layer4[1], pretrained_net.avgpool, pretrained_net.fc
