84da91a9a4bb2cc99a0ac3d51661065514d2ae2b
dae4 <3210m0123@gmail.com>
2021-06-04 22:34:04 +0900

    FCN.py

--- torch/Chapter13/FCN.py
+++ torch/Chapter13/FCN.py
@@ -1,3 +1,4 @@
+#%%
 import torch
 import torchvision
 from torch import nn
@@ -8,3 +9,14 @@ from func import *
 
 pretrained_net = torchvision.models.resnet18(pretrained=True)
 pretrained_net.layer4[1], pretrained_net.avgpool, pretrained_net.fc
+
+net = nn.Sequential(*list(pretrained_net.children())[:-2])
+
+X = torch.rand(size=(1, 3, 320, 480))
+net(X).shape
+#%%
+num_classes = 21
+net.add_module('final_conv', nn.Conv2d(512, num_classes, kernel_size=1))
+net.add_module('transpose_conv', nn.ConvTranspose2d(num_classes, num_classes,
+                                    kernel_size=64, padding=16, stride=32))
+# %%
