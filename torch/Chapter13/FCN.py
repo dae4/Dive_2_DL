06f8b83d9b2ca54d2175ea9c3164bdf15a89ae99
dae4 <3210m0123@gmail.com>
2021-06-05 23:40:02 +0900

    FCN.py

--- torch/Chapter13/FCN.py
+++ torch/Chapter13/FCN.py
@@ -3,7 +3,8 @@ import torch
 import torchvision
 from torch import nn
 from torch.nn import functional as F
-
+import PIL.Image as Image
+import matplotlib.pyplot as plt
 from func import *
 
 
@@ -19,4 +20,38 @@ num_classes = 21
 net.add_module('final_conv', nn.Conv2d(512, num_classes, kernel_size=1))
 net.add_module('transpose_conv', nn.ConvTranspose2d(num_classes, num_classes,
                                     kernel_size=64, padding=16, stride=32))
+# %%
+def bilinear_kernel(in_channels, out_channels, kernel_size):
+    factor = (kernel_size + 1) // 2
+    if kernel_size % 2 == 1:
+        center = factor - 1
+    else:
+        center = factor - 0.5
+    og = (torch.arange(kernel_size).reshape(-1, 1),
+          torch.arange(kernel_size).reshape(1, -1))
+    filt = (1 - torch.abs(og[0] - center) / factor) * \
+           (1 - torch.abs(og[1] - center) / factor)
+    weight = torch.zeros((in_channels, out_channels,
+                          kernel_size, kernel_size))
+    weight[range(in_channels), range(out_channels), :, :] = filt
+    return weight
+# %%
+conv_trans = nn.ConvTranspose2d(3, 3, kernel_size=4, padding=1, stride=2,
+                                bias=False)
+conv_trans.weight.data.copy_(bilinear_kernel(3, 3, 4));
+# %%
+img = torchvision.transforms.ToTensor()(Image.open('catdog.jpg'))
+X = img.unsqueeze(0)
+Y = conv_trans(X)
+out_img = Y[0].permute(1, 2, 0).detach()
+# %%
+set_figsize()
+print('input image shape:', img.permute(1, 2, 0).shape)
+plt.imshow(img.permute(1, 2, 0));
+print('output image shape:', out_img.shape)
+plt.imshow(out_img);
+# %%
+W = bilinear_kernel(num_classes, num_classes, 64)
+net.transpose_conv.weight.data.copy_(W);
+
 # %%
