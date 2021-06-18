9433eda12b7635eb32521472529173f7d5b052d1
dae4 <3210m0123@gmail.com>
2021-06-18 15:57:38 +0900

    The_Dataset_for_Pretraining_Word_Embedding.py

--- torch/Chapter14/The_Dataset_for_Pretraining_Word_Embedding.py
+++ torch/Chapter14/The_Dataset_for_Pretraining_Word_Embedding.py
@@ -58,3 +58,115 @@ compare_counts('join')
 corpus = [vocab[line] for line in subsampled]
 corpus[0:3]
 # %%
+## Loading the Dataset
+def get_centers_and_contexts(corpus, max_window_size):
+    centers, contexts = [], []
+    for line in corpus:
+        # Each sentence needs at least 2 words to form a "central target word
+        # - context word" pair
+        if len(line) < 2:
+            continue
+        centers += line
+        for i in range(len(line)):  # Context window centered at i
+            window_size = random.randint(1, max_window_size)
+            indices = list(range(max(0, i - window_size),
+                                 min(len(line), i + 1 + window_size)))
+            # Exclude the central target word from the context words
+            indices.remove(i)
+            contexts.append([line[idx] for idx in indices])
+    return centers, contexts
+# %%
+tiny_dataset = [list(range(7)), list(range(7, 10))]
+print('dataset', tiny_dataset)
+for center, context in zip(*get_centers_and_contexts(tiny_dataset, 2)):
+    print('center', center, 'has contexts', context)
+# %%
+all_centers, all_contexts = get_centers_and_contexts(corpus, 5)
+f'# center-context pairs: {len(all_centers)}'
+# %%
+## Negative Sampling
+class RandomGenerator:
+    """Draw a random int in [0, n] according to n sampling weights."""
+    def __init__(self, sampling_weights):
+        self.population = list(range(len(sampling_weights)))
+        self.sampling_weights = sampling_weights
+        self.candidates = []
+        self.i = 0
+
+    def draw(self):
+        if self.i == len(self.candidates):
+            self.candidates = random.choices(
+                self.population, self.sampling_weights, k=10000)
+            self.i = 0
+        self.i += 1
+        return self.candidates[self.i-1]
+
+generator = RandomGenerator([2, 3, 4])
+[generator.draw() for _ in range(10)]
+# %%
+def get_negatives(all_contexts, corpus, K):
+    counter = count_corpus(corpus)
+    sampling_weights = [count**0.75 for count in counter.values()]
+    all_negatives, generator = [], RandomGenerator(sampling_weights)
+    for contexts in all_contexts:
+        negatives = []
+        while len(negatives) < len(contexts) * K:
+            neg = generator.draw()
+            # Noise words cannot be context words
+            if neg not in contexts:
+                negatives.append(neg)
+        all_negatives.append(negatives)
+    return all_negatives
+
+all_negatives = get_negatives(all_contexts, corpus, 5)
+# %%
+def batchify(data):
+    max_len = max(len(c) + len(n) for _, c, n in data)
+    centers, contexts_negatives, masks, labels = [], [], [], []
+    for center, context, negative in data:
+        cur_len = len(context) + len(negative)
+        centers += [center]
+        contexts_negatives += [context + negative + [0] * (max_len - cur_len)]
+        masks += [[1] * cur_len + [0] * (max_len - cur_len)]
+        labels += [[1] * len(context) + [0] * (max_len - len(context))]
+    return (torch.reshape(torch.tensor(centers), (-1, 1)), torch.tensor(contexts_negatives),
+            torch.tensor(masks), torch.tensor(labels))
+# %%
+x_1 = (1, [2, 2], [3, 3, 3, 3])
+x_2 = (1, [2, 2, 2], [3, 3])
+batch = batchify((x_1, x_2))
+
+names = ['centers', 'contexts_negatives', 'masks', 'labels']
+for name, data in zip(names, batch):
+    print(name, '=', data)
+# %%
+def load_data_ptb(batch_size, max_window_size, num_noise_words):
+    num_workers = get_dataloader_workers()
+    sentences = read_ptb()
+    vocab = Vocab(sentences, min_freq=10)
+    subsampled = subsampling(sentences, vocab)
+    corpus = [vocab[line] for line in subsampled]
+    all_centers, all_contexts = get_centers_and_contexts(
+        corpus, max_window_size)
+    all_negatives = get_negatives(all_contexts, corpus, num_noise_words)
+
+    class PTBDataset(torch.utils.data.Dataset):
+        def __init__(self, centers, contexts, negatives):
+            assert len(centers) == len(contexts) == len(negatives)
+            self.centers = centers
+            self.contexts = contexts
+            self.negatives = negatives
+
+        def __getitem__(self, index):
+            return (self.centers[index], self.contexts[index], self.negatives[index])
+
+        def __len__(self):
+            return len(self.centers)
+
+    dataset = PTBDataset(
+        all_centers, all_contexts, all_negatives)
+
+    data_iter = torch.utils.data.DataLoader(dataset, batch_size, shuffle=True,
+                                      collate_fn=batchify,
+                                      num_workers=num_workers)
+    return data_iter, vocab
