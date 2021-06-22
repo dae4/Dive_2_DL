83be2cf7842f4205260c0d53e478222ded6cb15b
dae4 <3210m0123@gmail.com>
2021-06-22 20:09:46 +0900

    Finding_Synonyms_and_Analogies.py

---
+++ torch/Chapter14/Finding_Synonyms_and_Analogies.py
@@ -0,0 +1,81 @@
+#%%
+import torch
+from torch import nn
+import os
+from func import *
+
+class TokenEmbedding:
+    """Token Embedding."""
+    def __init__(self, embedding_name):
+        self.idx_to_token, self.idx_to_vec = self._load_embedding(
+            embedding_name)
+        self.unknown_idx = 0
+        self.token_to_idx = {token: idx for idx, token in
+                             enumerate(self.idx_to_token)}
+
+    def _load_embedding(self, embedding_name):
+        idx_to_token, idx_to_vec = ['<unk>'], []
+        data_dir = download_extract(embedding_name)
+        # GloVe website: https://nlp.stanford.edu/projects/glove/
+        # fastText website: https://fasttext.cc/
+        with open(os.path.join(data_dir, 'vec.txt'), 'r') as f:
+            for line in f:
+                elems = line.rstrip().split(' ')
+                token, elems = elems[0], [float(elem) for elem in elems[1:]]
+                # Skip header information, such as the top row in fastText
+                if len(elems) > 1:
+                    idx_to_token.append(token)
+                    idx_to_vec.append(elems)
+        idx_to_vec = [[0] * len(idx_to_vec[0])] + idx_to_vec
+        return idx_to_token, torch.tensor(idx_to_vec)
+
+    def __getitem__(self, tokens):
+        indices = [self.token_to_idx.get(token, self.unknown_idx)
+                   for token in tokens]
+        vecs = self.idx_to_vec[torch.tensor(indices)]
+        return vecs
+
+    def __len__(self):
+        return len(self.idx_to_token)
+
+glove_6b50d = TokenEmbedding('glove.6b.50d')
+#%%
+len(glove_6b50d)
+#%%
+glove_6b50d.token_to_idx['beautiful'], glove_6b50d.idx_to_token[3367]
+
+# %%
+def knn(W, x, k):
+    # The added 1e-9 is for numerical stability
+    cos = torch.mv(W, x.reshape(-1,)) / (
+        torch.sqrt(torch.sum(W * W, axis=1) + 1e-9) *
+        torch.sqrt((x * x).sum()))
+    _, topk = torch.topk(cos, k=k)
+    return topk, [cos[int(i)] for i in topk]
+# %%
+def get_similar_tokens(query_token, k, embed):
+    topk, cos = knn(embed.idx_to_vec, embed[[query_token]], k + 1)
+    for i, c in zip(topk[1:], cos[1:]):  # Remove input words
+        print(f'cosine sim={float(c):.3f}: {embed.idx_to_token[int(i)]}')
+# %%
+get_similar_tokens('chip', 3, glove_6b50d)
+# %%
+get_similar_tokens('baby', 3, glove_6b50d)
+# %%
+get_similar_tokens('beautiful', 3, glove_6b50d)
+# %%
+def get_analogy(token_a, token_b, token_c, embed):
+    vecs = embed[[token_a, token_b, token_c]]
+    x = vecs[1] - vecs[0] + vecs[2]
+    topk, cos = knn(embed.idx_to_vec, x, 1)
+    return embed.idx_to_token[int(topk[0])]  # Remove unknown words
+# %%
+get_analogy('man', 'woman', 'son', glove_6b50d)
+# %%
+get_analogy('beijing', 'china', 'tokyo', glove_6b50d)
+# %%
+get_analogy('bad', 'worst', 'big', glove_6b50d)
+# %%
+get_analogy('do', 'did', 'go', glove_6b50d)
+
+# %%
