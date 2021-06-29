64d28701ba7da2ceaeba907ec4439c9b80f38250
dae4 <3210m0123@gmail.com>
2021-06-30 03:35:35 +0900

    Sentiment_Analysis_and_the_Dataset.py

---
+++ torch/Chapter15/Sentiment_Analysis_and_the_Dataset.py
@@ -0,0 +1,61 @@
+#%%
+import torch
+from torch import nn
+import os
+from func import *
+import matplotlib.pyplot as plt
+
+data_dir = download_extract('aclImdb', 'aclImdb')
+
+def read_imdb(data_dir, is_train):
+    data, labels = [], []
+    for label in ('pos', 'neg'):
+        folder_name = os.path.join(data_dir, 'train' if is_train else 'test',
+                                   label)
+        for file in os.listdir(folder_name):
+            with open(os.path.join(folder_name, file), 'rb') as f:
+                review = f.read().decode('utf-8').replace('\n', '')
+                data.append(review)
+                labels.append(1 if label == 'pos' else 0)
+    return data, labels
+
+train_data = read_imdb(data_dir, is_train=True)
+print('# trainings:', len(train_data[0]))
+for x, y in zip(train_data[0][:3], train_data[1][:3]):
+    print('label:', y, 'review:', x[0:60])
+
+train_tokens = tokenize(train_data[0], token='word')
+vocab = Vocab(train_tokens, min_freq=5, reserved_tokens=['<pad>'])
+
+set_figsize()
+plt.hist([len(line) for line in train_tokens], bins=range(0, 1000, 50));
+
+num_steps = 500  # sequence length
+train_features = torch.tensor([truncate_pad(
+    vocab[line], num_steps, vocab['<pad>']) for line in train_tokens])
+print(train_features.shape)
+#%%
+train_iter = load_array((train_features, torch.tensor(train_data[1])), 64)
+
+for X, y in train_iter:
+    print('X:', X.shape, ', y:', y.shape)
+    break
+print('# batches:', len(train_iter))
+
+def load_data_imdb(batch_size, num_steps=500):
+    data_dir = download_extract('aclImdb', 'aclImdb')
+    train_data = read_imdb(data_dir, True)
+    test_data = read_imdb(data_dir, False)
+    train_tokens = tokenize(train_data[0], token='word')
+    test_tokens = tokenize(test_data[0], token='word')
+    vocab = Vocab(train_tokens, min_freq=5)
+    train_features = torch.tensor([truncate_pad(
+        vocab[line], num_steps, vocab['<pad>']) for line in train_tokens])
+    test_features = torch.tensor([truncate_pad(
+        vocab[line], num_steps, vocab['<pad>']) for line in test_tokens])
+    train_iter = load_array((train_features, torch.tensor(train_data[1])),
+                                batch_size)
+    test_iter = load_array((test_features, torch.tensor(test_data[1])),
+                               batch_size,
+                               is_train=False)
+    return train_iter, test_iter, vocab
