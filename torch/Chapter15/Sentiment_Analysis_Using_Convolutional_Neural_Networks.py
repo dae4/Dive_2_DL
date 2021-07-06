92347e555536fbcc4d1c3a34f45f75d2c85aad45
dae4 <3210m0123@gmail.com>
2021-07-06 23:24:48 +0900

    Sentiment_Analysis_Using_Convolutional_Neural_Networks.py

--- torch/Chapter15/Sentiment_Analysis_Using_Convolutional_Neural_Networks.py
+++ torch/Chapter15/Sentiment_Analysis_Using_Convolutional_Neural_Networks.py
@@ -1,5 +1,14 @@
 import torch
 from torch import nn
+from func import *
+
 
 batch_size = 64
 train_iter, test_iter, vocab = load_data_imdb(batch_size)
+
+def corr1d(X, K):
+    w = K.shape[0]
+    Y = torch.zeros((X.shape[0] - w + 1))
+    for i in range(Y.shape[0]):
+        Y[i] = (X[i: i + w] * K).sum()
+    return Y
