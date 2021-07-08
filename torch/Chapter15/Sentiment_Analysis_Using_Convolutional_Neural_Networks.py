b256955c13e307e0243d655fa0b6517638fb7513
dae4 <3210m0123@gmail.com>
2021-07-08 18:21:27 +0900

    Sentiment_Analysis_Using_Convolutional_Neural_Networks.py

--- torch/Chapter15/Sentiment_Analysis_Using_Convolutional_Neural_Networks.py
+++ torch/Chapter15/Sentiment_Analysis_Using_Convolutional_Neural_Networks.py
@@ -27,3 +27,43 @@ X = torch.tensor([[0, 1, 2, 3, 4, 5, 6],
               [2, 3, 4, 5, 6, 7, 8]])
 K = torch.tensor([[1, 2], [3, 4], [-1, -3]])
 corr1d_multi_in(X, K)
+
+#%%
+class TextCNN(nn.Module):
+    def __init__(self, vocab_size, embed_size, kernel_sizes, num_channels,
+                 **kwargs):
+        super(TextCNN, self).__init__(**kwargs)
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        # The embedding layer does not participate in training
+        self.constant_embedding = nn.Embedding(vocab_size, embed_size)
+        self.dropout = nn.Dropout(0.5)
+        self.decoder = nn.Linear(sum(num_channels), 2)
+        # The max-over-time pooling layer has no weight, so it can share an
+        # instance
+        self.pool = nn.AdaptiveAvgPool1d(1)
+        self.relu = nn.ReLU()
+        # Create multiple one-dimensional convolutional layers
+        self.convs = nn.ModuleList()
+        for c, k in zip(num_channels, kernel_sizes):
+            self.convs.append(nn.Conv1d(2 * embed_size, c, k))
+
+    def forward(self, inputs):
+        # Concatenate the output of two embedding layers with shape of
+        # (batch size, no. of words, word vector dimension) by word vector
+        embeddings = torch.cat((
+            self.embedding(inputs), self.constant_embedding(inputs)), dim=2)
+        # According to the input format required by Conv1d, the word vector
+        # dimension, that is, the channel dimension of the one-dimensional
+        # convolutional layer, is transformed into the previous dimension
+        embeddings = embeddings.permute(0, 2, 1)
+        # For each one-dimensional convolutional layer, after max-over-time
+        # pooling, a tensor with the shape of (batch size, channel size, 1)
+        # can be obtained. Use the flatten function to remove the last
+        # dimension and then concatenate on the channel dimension
+        encoding = torch.cat([
+            torch.squeeze(self.relu(self.pool(conv(embeddings))), dim=-1)
+            for conv in self.convs], dim=1)
+        # After applying the dropout method, use a fully connected layer to
+        # obtain the output
+        outputs = self.decoder(self.dropout(encoding))
+        return outputs
