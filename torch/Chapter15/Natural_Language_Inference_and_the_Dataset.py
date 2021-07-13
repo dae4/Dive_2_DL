b1c1ec9125e5257271c108ca20d8679296041f2b
dae4 <3210m0123@gmail.com>
2021-07-13 20:49:00 +0900

    Natural_Language_Inference_and_the_Dataset.py

--- torch/Chapter15/Natural_Language_Inference_and_the_Dataset.py
+++ torch/Chapter15/Natural_Language_Inference_and_the_Dataset.py
@@ -24,3 +24,72 @@ def read_snli(data_dir, is_train):
     hypotheses = [extract_text(row[2]) for row in rows if row[0] in label_set]
     labels = [label_set[row[0]] for row in rows if row[0] in label_set]
     return premises, hypotheses, labels
+
+#%%
+train_data = read_snli(data_dir, is_train=True)
+for x0, x1, y in zip(train_data[0][:3], train_data[1][:3], train_data[2][:3]):
+    print('premise:', x0)
+    print('hypothesis:', x1)
+    print('label:', y)
+
+#%%
+test_data = read_snli(data_dir, is_train=False)
+for data in [train_data, test_data]:
+    print([[row for row in data[2]].count(i) for i in range(3)])
+
+#%%
+class SNLIDataset(torch.utils.data.Dataset):
+    """A customized dataset to load the SNLI dataset."""
+    def __init__(self, dataset, num_steps, vocab=None):
+        self.num_steps = num_steps
+        all_premise_tokens = tokenize(dataset[0])
+        all_hypothesis_tokens = tokenize(dataset[1])
+        if vocab is None:
+            self.vocab = Vocab(all_premise_tokens + all_hypothesis_tokens,
+                                   min_freq=5, reserved_tokens=['<pad>'])
+        else:
+            self.vocab = vocab
+        self.premises = self._pad(all_premise_tokens)
+        self.hypotheses = self._pad(all_hypothesis_tokens)
+        self.labels = torch.tensor(dataset[2])
+        print('read ' + str(len(self.premises)) + ' examples')
+
+    def _pad(self, lines):
+        return torch.tensor([truncate_pad(
+            self.vocab[line], self.num_steps, self.vocab['<pad>'])
+                         for line in lines])
+
+    def __getitem__(self, idx):
+        return (self.premises[idx], self.hypotheses[idx]), self.labels[idx]
+
+    def __len__(self):
+        return len(self.premises)
+
+
+#%%
+def load_data_snli(batch_size, num_steps=50):
+    """Download the SNLI dataset and return data iterators and vocabulary."""
+    num_workers = get_dataloader_workers()
+    data_dir = download_extract('SNLI')
+    train_data = read_snli(data_dir, True)
+    test_data = read_snli(data_dir, False)
+    train_set = SNLIDataset(train_data, num_steps)
+    test_set = SNLIDataset(test_data, num_steps, train_set.vocab)
+    train_iter = torch.utils.data.DataLoader(train_set, batch_size,
+                                             shuffle=True,
+                                             num_workers=num_workers)
+    test_iter = torch.utils.data.DataLoader(test_set, batch_size,
+                                            shuffle=False,
+                                            num_workers=num_workers)
+    return train_iter, test_iter, train_set.vocab
+
+#%%
+train_iter, test_iter, vocab = load_data_snli(128, 50)
+len(vocab)
+#%%
+for X, Y in train_iter:
+    print(X[0].shape)
+    print(X[1].shape)
+    print(Y.shape)
+    break
+#%%
