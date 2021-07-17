e4c5d1fe7c0ee2971bb531a2652990af0a96eb3b
dae4 <3210m0123@gmail.com>
2021-07-17 19:17:26 +0900

    Natural_Language_Inference_Using_Attention.py

--- torch/Chapter15/Natural_Language_Inference_Using_Attention.py
+++ torch/Chapter15/Natural_Language_Inference_Using_Attention.py
@@ -43,5 +43,79 @@ class Attend(nn.Module):
         alpha = torch.bmm(F.softmax(e.permute(0, 2, 1), dim=-1), A)
         return beta, alpha
 
+#%%
+class Compare(nn.Module):
+    def __init__(self, num_inputs, num_hiddens, **kwargs):
+        super(Compare, self).__init__(**kwargs)
+        self.g = mlp(num_inputs, num_hiddens, flatten=False)
+
+    def forward(self, A, B, beta, alpha):
+        V_A = self.g(torch.cat([A, beta], dim=2))
+        V_B = self.g(torch.cat([B, alpha], dim=2))
+        return V_A, V_B
+#%%
+class Aggregate(nn.Module):
+    def __init__(self, num_inputs, num_hiddens, num_outputs, **kwargs):
+        super(Aggregate, self).__init__(**kwargs)
+        self.h = mlp(num_inputs, num_hiddens, flatten=True)
+        self.linear = nn.Linear(num_hiddens, num_outputs)
+
+    def forward(self, V_A, V_B):
+        # Sum up both sets of comparison vectors
+        V_A = V_A.sum(dim=1)
+        V_B = V_B.sum(dim=1)
+        # Feed the concatenation of both summarization results into an MLP
+        Y_hat = self.linear(self.h(torch.cat([V_A, V_B], dim=1)))
+        return Y_hat
+#%%
+class DecomposableAttention(nn.Module):
+    def __init__(self, vocab, embed_size, num_hiddens, num_inputs_attend=100,
+                 num_inputs_compare=200, num_inputs_agg=400, **kwargs):
+        super(DecomposableAttention, self).__init__(**kwargs)
+        self.embedding = nn.Embedding(len(vocab), embed_size)
+        self.attend = Attend(num_inputs_attend, num_hiddens)
+        self.compare = Compare(num_inputs_compare, num_hiddens)
+        # There are 3 possible outputs: entailment, contradiction, and neutral
+        self.aggregate = Aggregate(num_inputs_agg, num_hiddens, num_outputs=3)
+
+    def forward(self, X):
+        premises, hypotheses = X
+        A = self.embedding(premises)
+        B = self.embedding(hypotheses)
+        beta, alpha = self.attend(A, B)
+        V_A, V_B = self.compare(A, B, beta, alpha)
+        Y_hat = self.aggregate(V_A, V_B)
+        return Y_hat
 
 #%%
+
+batch_size, num_steps = 256, 50
+train_iter, test_iter, vocab = load_data_snli(batch_size, num_steps)
+
+#%%
+embed_size, num_hiddens, devices = 100, 200, try_all_gpus()
+net = DecomposableAttention(vocab, embed_size, num_hiddens)
+glove_embedding = TokenEmbedding('glove.6b.100d')
+embeds = glove_embedding[vocab.idx_to_token]
+net.embedding.weight.data.copy_(embeds)
+
+#%%
+
+lr, num_epochs = 0.001, 4
+trainer = torch.optim.Adam(net.parameters(), lr=lr)
+loss = nn.CrossEntropyLoss(reduction="none")
+train_ch13(net, train_iter, test_iter, loss, trainer, num_epochs, devices)
+
+#%%
+def predict_snli(net, vocab, premise, hypothesis):
+    net.eval()
+    premise = torch.tensor(vocab[premise], device=try_gpu())
+    hypothesis = torch.tensor(vocab[hypothesis], device=try_gpu())
+    label = torch.argmax(net([premise.reshape((1, -1)),
+                           hypothesis.reshape((1, -1))]), dim=1)
+    return 'entailment' if label == 0 else 'contradiction' if label == 1 \
+            else 'neutral'
+
+#%%
+
+predict_snli(net, vocab, ['he', 'is', 'good', '.'], ['he', 'is', 'bad', '.'])
--- torch/Chapter15/func.py
+++ torch/Chapter15/func.py
@@ -1429,3 +1429,76 @@ def predict_sentiment(net, vocab, sentence):
     sentence = torch.tensor(vocab[sentence.split()], device=try_gpu())
     label = torch.argmax(net(sentence.reshape(1, -1)), dim=1)
     return 'positive' if label == 1 else 'negative'
+
+def read_snli(data_dir, is_train):
+    """Read the SNLI dataset into premises, hypotheses, and labels."""
+    def extract_text(s):
+        # Remove information that will not be used by us
+        s = re.sub('\\(', '', s) 
+        s = re.sub('\\)', '', s)
+        # Substitute two or more consecutive whitespace with space
+        s = re.sub('\\s{2,}', ' ', s)
+        return s.strip()
+    label_set = {'entailment': 0, 'contradiction': 1, 'neutral': 2}
+    file_name = os.path.join(data_dir, 'snli_1.0_train.txt'
+                             if is_train else 'snli_1.0_test.txt')
+    with open(file_name, 'r') as f:
+        rows = [row.split('\t') for row in f.readlines()[1:]]
+    premises = [extract_text(row[1]) for row in rows if row[0] in label_set]
+    hypotheses = [extract_text(row[2]) for row in rows if row[0] in label_set]
+    labels = [label_set[row[0]] for row in rows if row[0] in label_set]
+    return premises, hypotheses, labels
+
+class SNLIDataset(torch.utils.data.Dataset):
+    """A customized dataset to load the SNLI dataset."""
+    def __init__(self, dataset, num_steps, vocab=None):
+        self.num_steps = num_steps
+        all_premise_tokens = tokenize(dataset[0])
+        all_hypothesis_tokens = tokenize(dataset[1])
+        if vocab is None:
+            self.vocab = Vocab(all_premise_tokens + all_hypothesis_tokens,
+                                   min_freq=5, reserved_tokens=['<pad>'])
+        else:
+            self.vocab = vocab
+        self.premises = self._pad(all_premise_tokens)
+        self.hypotheses = self._pad(all_hypothesis_tokens)
+        self.labels = torch.tensor(dataset[2])
+        print('read ' + str(len(self.premises)) + ' examples')
+
+    def _pad(self, lines):
+        return torch.tensor([truncate_pad(
+            self.vocab[line], self.num_steps, self.vocab['<pad>'])
+                         for line in lines])
+
+    def __getitem__(self, idx):
+        return (self.premises[idx], self.hypotheses[idx]), self.labels[idx]
+
+    def __len__(self):
+        return len(self.premises)
+
+
+def load_data_snli(batch_size, num_steps=50):
+    """Download the SNLI dataset and return data iterators and vocabulary."""
+    num_workers = get_dataloader_workers()
+    data_dir = download_extract('SNLI')
+    train_data = read_snli(data_dir, True)
+    test_data = read_snli(data_dir, False)
+    train_set = SNLIDataset(train_data, num_steps)
+    test_set = SNLIDataset(test_data, num_steps, train_set.vocab)
+    train_iter = torch.utils.data.DataLoader(train_set, batch_size,
+                                             shuffle=True,
+                                             num_workers=num_workers)
+    test_iter = torch.utils.data.DataLoader(test_set, batch_size,
+                                            shuffle=False,
+                                            num_workers=num_workers)
+    return train_iter, test_iter, train_set.vocab
+
+
+def predict_snli(net, vocab, premise, hypothesis):
+    net.eval()
+    premise = torch.tensor(vocab[premise], device=try_gpu())
+    hypothesis = torch.tensor(vocab[hypothesis], device=try_gpu())
+    label = torch.argmax(net([premise.reshape((1, -1)),
+                           hypothesis.reshape((1, -1))]), dim=1)
+    return 'entailment' if label == 0 else 'contradiction' if label == 1 \
+            else 'neutral'
