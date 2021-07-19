b0448520822c9e27029e98ae5967e6bece13bcdf
dae4 <3210m0123@gmail.com>
2021-07-19 20:17:22 +0900

    Natural_Language_Inference_Fine-Tuning_BERT.py

--- torch/Chapter15/Natural_Language_Inference_Fine-Tuning_BERT.py
+++ torch/Chapter15/Natural_Language_Inference_Fine-Tuning_BERT.py
@@ -1,6 +1,25 @@
+#%%
 import json
 import multiprocessing
 import torch
 from torch import nn
 import os
 from func import *
+#%%
+def load_pretrained_model(pretrained_model, num_hiddens, ffn_num_hiddens,
+                          num_heads, num_layers, dropout, max_len, devices):
+    data_dir = download_extract(pretrained_model)
+    # Define an empty vocabulary to load the predefined vocabulary
+    vocab = Vocab()
+    vocab.idx_to_token = json.load(open(os.path.join(data_dir, 'vocab.json')))
+    vocab.token_to_idx = {token: idx for idx, token in enumerate(
+        vocab.idx_to_token)}
+    bert = BERTModel(len(vocab), num_hiddens, norm_shape=[256],
+                         ffn_num_input=256, ffn_num_hiddens=ffn_num_hiddens,
+                         num_heads=4, num_layers=2, dropout=0.2,
+                         max_len=max_len, key_size=256, query_size=256,
+                         value_size=256, hid_in_features=256,
+                         mlm_in_features=256, nsp_in_features=256)
+    # Load pretrained BERT parameters
+    bert.load_state_dict(torch.load(os.path.join(data_dir,'pretrained.params')))
+    return bert, vocab
