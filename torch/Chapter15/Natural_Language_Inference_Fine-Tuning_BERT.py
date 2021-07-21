e20dba3f77b5e66d3a21f9300dd747c39779e25f
dae4 <3210m0123@gmail.com>
2021-07-21 19:24:21 +0900

    Natural_Language_Inference_Fine-Tuning_BERT.py

--- torch/Chapter15/Natural_Language_Inference_Fine-Tuning_BERT.py
+++ torch/Chapter15/Natural_Language_Inference_Fine-Tuning_BERT.py
@@ -25,3 +25,90 @@ def load_pretrained_model(pretrained_model, num_hiddens, ffn_num_hiddens,
     return bert, vocab
 
 #%%
+devices = try_all_gpus()
+bert, vocab = load_pretrained_model(
+    'bert.small', num_hiddens=256, ffn_num_hiddens=512, num_heads=4,
+    num_layers=2, dropout=0.1, max_len=512, devices=devices)
+#%%
+class SNLIBERTDataset(torch.utils.data.Dataset):
+    def __init__(self, dataset, max_len, vocab=None):
+        all_premise_hypothesis_tokens = [[
+            p_tokens, h_tokens] for p_tokens, h_tokens in zip(
+            *[d2l.tokenize([s.lower() for s in sentences])
+              for sentences in dataset[:2]])]
+        
+        self.labels = torch.tensor(dataset[2])
+        self.vocab = vocab
+        self.max_len = max_len
+        (self.all_token_ids, self.all_segments,
+         self.valid_lens) = self._preprocess(all_premise_hypothesis_tokens)
+        print('read ' + str(len(self.all_token_ids)) + ' examples')
+
+    def _preprocess(self, all_premise_hypothesis_tokens):
+        pool = multiprocessing.Pool(4)  # Use 4 worker processes
+        out = pool.map(self._mp_worker, all_premise_hypothesis_tokens)
+        all_token_ids = [
+            token_ids for token_ids, segments, valid_len in out]
+        all_segments = [segments for token_ids, segments, valid_len in out]
+        valid_lens = [valid_len for token_ids, segments, valid_len in out]
+        return (torch.tensor(all_token_ids, dtype=torch.long),
+                torch.tensor(all_segments, dtype=torch.long), 
+                torch.tensor(valid_lens))
+
+    def _mp_worker(self, premise_hypothesis_tokens):
+        p_tokens, h_tokens = premise_hypothesis_tokens
+        self._truncate_pair_of_tokens(p_tokens, h_tokens)
+        tokens, segments = get_tokens_and_segments(p_tokens, h_tokens)
+        token_ids = self.vocab[tokens] + [self.vocab['<pad>']] \
+                             * (self.max_len - len(tokens))
+        segments = segments + [0] * (self.max_len - len(segments))
+        valid_len = len(tokens)
+        return token_ids, segments, valid_len
+
+    def _truncate_pair_of_tokens(self, p_tokens, h_tokens):
+        # Reserve slots for '<CLS>', '<SEP>', and '<SEP>' tokens for the BERT
+        # input
+        while len(p_tokens) + len(h_tokens) > self.max_len - 3:
+            if len(p_tokens) > len(h_tokens):
+                p_tokens.pop()
+            else:
+                h_tokens.pop()
+
+    def __getitem__(self, idx):
+        return (self.all_token_ids[idx], self.all_segments[idx],
+                self.valid_lens[idx]), self.labels[idx]
+
+    def __len__(self):
+        return len(self.all_token_ids)
+
+#%%
+# Reduce `batch_size` if there is an out of memory error. In the original BERT
+# model, `max_len` = 512
+batch_size, max_len, num_workers = 512, 128, get_dataloader_workers()
+data_dir = download_extract('SNLI')
+train_set = SNLIBERTDataset(read_snli(data_dir, True), max_len, vocab)
+test_set = SNLIBERTDataset(read_snli(data_dir, False), max_len, vocab)
+train_iter = torch.utils.data.DataLoader(train_set, batch_size, shuffle=True,
+                                   num_workers=num_workers)
+test_iter = torch.utils.data.DataLoader(test_set, batch_size,
+                                  num_workers=num_workers)
+
+#%%
+class BERTClassifier(nn.Module):
+    def __init__(self, bert):
+        super(BERTClassifier, self).__init__()
+        self.encoder = bert.encoder
+        self.hidden = bert.hidden
+        self.output = nn.Linear(256, 3)
+
+    def forward(self, inputs):
+        tokens_X, segments_X, valid_lens_x = inputs
+        encoded_X = self.encoder(tokens_X, segments_X, valid_lens_x)
+        return self.output(self.hidden(encoded_X[:, 0, :]))
+#%%
+net = BERTClassifier(bert)
+#%%
+lr, num_epochs = 1e-4, 5
+trainer = torch.optim.Adam(net.parameters(), lr=lr)
+loss = nn.CrossEntropyLoss(reduction='none')
+train_ch13(net, train_iter, test_iter, loss, trainer, num_epochs, devices)
